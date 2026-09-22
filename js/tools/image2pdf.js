6582b51b5716cf0bbe6f294d5efdef9ebeb8a8b4
jony99jrn <imjonyroy@gmail.com>
2026-09-23 00:39:47 +0600

    Create image2pdf.js

---
+++ js/tools/image2pdf.js
@@ -0,0 +1,11 @@
+window.TOOLS = window.TOOLS || [];
+
+window.TOOLS.push({
+  name: "Image To PDF",
+  description: "Convert your images into pdf...easy to use",
+  category: "pdf",
+  url: "https://image2pdf-jrn.vercel.app",
+  icon: ICON_MY_TOOL,
+  keywords: ["pdf", "image to pdf"],
+  featured: true
+});
